449c44067747e0109d4f0b7343f3fd00f72045a7
Wasif <wasifraza8011@gmail.com>
2024-04-09 02:09:47 +0500

    my angular project design fitoor app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './MyComponents/heroes/heroes.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { FiltersComponent } from './MyComponents/filters/filters.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
+import { OffersComponent } from './MyComponents/offers/offers.component';
+
+
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    FiltersComponent,
+    NavbarComponent,
+    FooterComponent,
+    OffersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzGridModule,
+    NzAutocompleteModule,
+    NzIconModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
